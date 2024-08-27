001f697366e2f331169be5542d486906ec796fa6
claramamute <claraflor22@outlook.com>
2024-08-27 16:56:59 -0300

    Criação do componente navbar e footer

---
+++ vital/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'green-1': '#87ce6e',
+      "green-2": "#437228",
+      'green-3': '#3F6212'
+    },
+   
+    extend: {},
+  },
+  plugins: [],
+}
+
