06bbfbe9effa66f9dc3576c547b486a8ec780420
TayluanSantos <tayluanjesus@gmail.com>
2024-11-04 17:48:57 -0300

    refactor: Refatorando o componente ListaProdutos

--- vital/src/components/produtos/listaprodutos/ListaProdutos.tsx
+++ vital/src/components/produtos/listaprodutos/ListaProdutos.tsx
@@ -1,67 +1,68 @@
-import { useEffect, useState } from "react"
-import { buscar } from "../../../services/Service"
-import Produto from "../../../models/Produto"
-import CardProduto from "../cardproduto/CardProduto"
-import { useNavigate } from "react-router-dom"
+import { useContext, useEffect, useState } from "react";
+import { buscar } from "../../../services/Service";
+import Produto from "../../../models/Produto";
+import CardProduto from "../cardproduto/CardProduto";
+import { useNavigate } from "react-router-dom";
 import { ToastAlerta } from "../../../utils/ToastAlerta";
 import { Link } from "react-router-dom";
-import { PlusCircle } from "@phosphor-icons/react"
+import { PlusCircle } from "@phosphor-icons/react";
+import AuthContext from "../../../contexts/AuthContext";
 
 function ListaProdutos() {
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const [produtos, setProdutos] = useState<Produto[]>([])
-
-  const navigate = useNavigate()
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+  const navigate = useNavigate();
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
 
   async function buscarProdutos() {
-
     try {
       await buscar(`/produtos`, (produtosOrdenados: Produto[]) => {
-        const listaProdutos = produtosOrdenados.sort((a, b) => a.id - b.id)
+        const listaProdutos = produtosOrdenados.sort((a, b) => a.id - b.id);
         setProdutos(listaProdutos);
-      }
-      )
-
+      });
     } catch (error) {
-      ToastAlerta('Houve um erro!', 'erro');
+      alert("Houve um erro!");
       navigate("/");
-
     }
-
   }
 
   useEffect(() => {
-    buscarProdutos()
-  }, [produtos.length])
+    buscarProdutos();
+  }, [produtos.length]);
 
   return (
-    <div className="bg-green-2 min-h-screen pt-9 px-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+    <div className="bg-[#F0F0F0] min-h-screen">
+      <div className="bg-green-1 px-6 py-5 flex flex-col gap-y-3 md:flex-row justify-between">
         <div>
-          <h2 className="text-3xl font-bold text-white">Escolha ou cadastre seu serviço</h2>
-          <p className="text-xl text-white mb-3">Encontre o profissional que melhor se adequa as suas necessidades</p>
+          <h2 className="text-2xl font-bold">Profissionais de Saúde</h2>
+          <p className="font-text text-sm">
+            Busque profissionais de saúde de acordo com cada especialidade
+          </p>
         </div>
-        <div>
-          <Link to={`/cadastroProduto`}>
-            <button className="flex items-center gap-x-1 border rounded font-semibold bg-white text-green-3 py-2 px-3  hover:shadow-lg hover:border-none">
-              <PlusCircle size={32} color="#437228" />
-              <span>Cadastrar</span>
-            </button>
-          </Link>
-        </div>
+        <Link to="/cadastroProduto">
+          <button
+            className={`${
+              token === "" || usuario.tipo === 1
+                ? "hidden"
+                : "px-5 py-2 bg-white rounded-lg font-semibold flex items-center gap-x-2 hover:scale-105 duration-300 ease-in-out"
+            }`}
+          >
+            <PlusCircle size={32} color="#000000" />
+            <span className="font-semibold text-center">Cadastrar Serviço</span>
+          </button>
+        </Link>
       </div>
-      <div className="container mx-auto py-12 grid grid-cols-1 gap-14">
+      <div className="flex flex-col gap-y-6 justify-center items-center p-8">
         {produtos.map((produto) => (
           <CardProduto key={produto.id} produto={produto} />
         ))}
       </div>
-     
     </div>
-  )
+  );
 }
 
-export default ListaProdutos
+export default ListaProdutos;
