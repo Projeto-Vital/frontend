5fa85513a1f7c04217f136ebc7a0b325a6753eec
TayluanSantos <tayluanjesus@gmail.com>
2024-11-05 11:58:26 -0300

    fix: Corrigindo a responsividade da Navbar

--- vital/src/components/navbar/Navbar.tsx
+++ vital/src/components/navbar/Navbar.tsx
@@ -51,13 +51,13 @@ function Navbar() {
           </div>
 
           <div className="flex items-center">
-            <div className="md:hidden">
+            <div className="lg:hidden">
               <button onClick={toggleMenu}>
                 {isOpen ? <X size={32} /> : <List size={32} />}
               </button>
             </div>
 
-            <div className="hidden md:flex items-center">
+            <div className="hidden lg:flex items-center">
               <div>
                 <UserCircle size={32} />
               </div>
@@ -82,7 +82,7 @@ function Navbar() {
             </div>
           </div>
   
-          <ul className={`md:hidden space-y-4 px-4 mt-16 py-7 bg-green-2 ${isOpen ? 'block fixed top-0 right-0 left-0 z-10' : 'hidden'}`}>
+          <ul className={`lg:hidden space-y-4 px-4 mt-16 py-7 bg-green-2 ${isOpen ? 'block fixed top-0 right-0 left-0 z-10' : 'hidden'}`}>
             <li className="hover:underline cursor-pointer text-white" onClick={toggleMenu}>
               <Link to={'/produtos'}>Serviços</Link>
             </li>
